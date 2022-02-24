f2b5fa196dcf7c853dede50890c4853e5a3dedc9
Dmitry Casian <webcasian@gmail.com>
2022-02-24 05:56:23 +0200

    Add invoke method for delay after crash and finish

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,13 +1,21 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private float delayAfterCrash;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Ground"))
         {
-            SceneManager.LoadScene(0);
+            Invoke(nameof(ReloadScene), delayAfterCrash);
         }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,11 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private float delayAfterFinish;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
-            SceneManager.LoadScene(0);
+            Invoke(nameof(ReloadScene), delayAfterFinish);
         }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
