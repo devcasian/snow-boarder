4ed23da48536b7d4961b8bd15c01ff80b484b70e
Dmitry Casian <webcasian@gmail.com>
2022-02-24 06:27:01 +0200

    Add triggers to particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,12 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [Header("Delay Settings")] 
     [SerializeField] private float delayAfterCrash;
+
+    [Header("Particle System Settings")] 
+    [SerializeField] private ParticleSystem crashEffect;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Ground"))
         {
+            crashEffect.Play();
             Invoke(nameof(ReloadScene), delayAfterCrash);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,12 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [Header("Delay Settings")] 
     [SerializeField] private float delayAfterFinish;
+
+    [Header("Particle System Settings")] 
+    [SerializeField] private ParticleSystem finishEffect;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
+            finishEffect.Play();
             Invoke(nameof(ReloadScene), delayAfterFinish);
         }
     }
