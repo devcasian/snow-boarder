80b0bc0b1f7c6d341d73c575376ae59c7046ff5e
Dmitry Casian <webcasian@gmail.com>
2022-02-24 04:59:03 +0200

    Add basic rotate mechanic to the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float torqueAmount;
+
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rb.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rb.AddTorque(-torqueAmount);
+        }
+    }
+}
