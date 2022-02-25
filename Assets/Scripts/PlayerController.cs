3a2e62e397c31bac1c80784042465a7f4be2c40e
Dmitry Casian <webcasian@gmail.com>
2022-02-25 02:55:29 +0200

    Add boost/slow control and level design polish

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float torqueAmount;
+    [SerializeField] private float boostSpeed;
+    [SerializeField] private float slowSpeed;
+
+    [SerializeField] private float baseSpeed;
+    // private const float ConstSpeed = 2f;
 
     private Rigidbody2D _rb;
+    private SurfaceEffector2D _surfaceEffector2D;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     private void Update()
+    {
+        RotatePlayer();
+        AddBoostAndSlow();
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -20,6 +33,22 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             _rb.AddTorque(-torqueAmount);
+        }
+    }
+
+    private void AddBoostAndSlow()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _surfaceEffector2D.speed = boostSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _surfaceEffector2D.speed = slowSpeed;
+        }
+        else
+        {
+            _surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
